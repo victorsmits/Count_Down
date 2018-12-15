bb7490a9e55410dd258479ff4804e4c7367b1910
victorsmits <victor.smits@student.vinci.be>
2018-12-15 15:04:09 +0100

    first commit

---
+++ Count_Down/CountDown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Timers;
+
+namespace Count_Down
+{
+    public class CountDown
+    {
+        private int seconde;
+
+        public CountDown(int sec)
+        {
+            this.seconde = sec;
+        }
+
+        public int RemainingTime
+        {
+            get
+            {
+                return seconde;
+            }
+        }
+
+        public void Start()
+        {
+            Timer aTimer = new Timer(1000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            seconde--;
+            Console.WriteLine(RemainingTime);
+        }
+    }
+}
---
+++ Count_Down/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Count_Down
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CountDown countdown = new CountDown(600);
+
+            countdown.Start();
+            Console.WriteLine(countdown.RemainingTime);
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine(countdown.RemainingTime);
+        }
+    }
+}
